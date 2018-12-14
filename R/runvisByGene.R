6f223a816f537c06e66f8571019ecfabeb87ce73
shwetagopaul <mail@shwetagopaul92.com>
2018-12-14 12:31:33 -0500

    code style changes

--- R/runvisByGene.R
+++ R/runvisByGene.R
@@ -1,7 +1,7 @@
 ######## Shiny APP ########
 ### visByGene ###
 
-#'shiny interface to Kimbie's CISBP FIMO scan TF bed files
+#'shiny interface to eQTL,HS,FP,TF files
 #'@import shiny
 #'@export
 runvisByGene<-function(){
